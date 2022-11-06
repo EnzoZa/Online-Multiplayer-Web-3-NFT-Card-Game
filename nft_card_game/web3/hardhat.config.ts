259019d82514e50bae0149d567bb41ea632b3699
EnzoZa <71082476+EnzoZa@users.noreply.github.com>
2022-11-06 13:15:38 +0100

    :tada::sparkles::construction::wrench: First/Initialize commit
    Initialize project with web3 and client

---
+++ nft_card_game/web3/hardhat.config.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import '@nomiclabs/hardhat-ethers';
+
+dotenv.config();
+
+export default {
+  solidity: {
+    version: '0.8.16',
+    settings: {
+      viaIR: true,
+      optimizer: {
+        enabled: true,
+        runs: 100,
+      },
+    },
+  },
+  networks: {
+    fuji: {
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      gasPrice: 225000000000,
+      chainId: 43113,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+}
